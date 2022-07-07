2f5297ddaf56f13b2be6111dd734ec56361fda30
Andresga17 <mariaerq10@gmail.com>
2022-07-07 17:46:53 -0500

    Clases estaticas anidadas. Creando una agenda de citas se creó la clase estatica AvailableAppointment dentro de la clase Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.Date;
 public class Doctor {
     //Atributos de la clase Doctor
     static int id = 0;//Siempre será autoincrementado
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private String speciality;
 
     //Declarando el metodo constructor dentro de la clase
     //se puede realizar un accion al llamar el metodo; como se muestra aqui.
@@ -27,4 +29,67 @@ public class Doctor {
     public void showId(){
         System.out.println("ID Doctor: " + id);//metodo que imprime el id de cada doctor
     }
+    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();//Declarando un objeto de tipo ArrayList
+    public void addAvailableAppointment(Date date, String time){//Metodo que asigna las nuevas citas a un array list
+        availableAppointments.add(new Doctor.AvailableAppointment(date,time));
+    }
+
+    public ArrayList<AvailableAppointment> getAvailableAppointments() { //Devuelve la lista completa de las citas
+        return availableAppointments;
+    }
+
+    //Definiendo set y get de name
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    //Definiendo set y get de speciality
+    public String getSpeciality() {
+        return this.speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    //Clase anidada
+    public static class AvailableAppointment{
+        private int id;
+        private Date date;
+        private String time;
+        //Metodo constructor
+        public AvailableAppointment(Date date, String time) { //Parametros minimos para que cree una cita disponible
+            this.date = date;
+            this.time = time;
+        }
+        //get y set de id
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        //get y set de Date
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+        //get y set de time
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -16,14 +18,24 @@ public class Main {
         //showMenu();
         //Creando un objeto tipo doctor con los parametros minimos necesarios asignados en el metodo constructor de la clase Doctor
         Doctor myDoctor = new Doctor("Andres Gonzalez", "Neurologia");
-        System.out.println("Nombre del doctor: " + myDoctor.name);
-        System.out.println("email: " + myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(),"4:00 p.m"); //Añadiendo nuevas citas a este doctor
+        myDoctor.addAvailableAppointment(new Date(),"10:00 a.m");
+        myDoctor.addAvailableAppointment(new Date(),"1:00 p.m");
+
+        for (Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate() + " " + aA.getTime());
+        }
+
+        System.out.println("Nombre del doctor: " + myDoctor.getName());
+        System.out.println("email: " + myDoctor.getSpeciality());
 
         System.out.println();
+
 //Creando un objeto de tipo patient
         Patient patient = new Patient("Alejandra", "alejandra@mail.com");
         patient.setWeight(54.6);
         System.out.println(patient.getWeight());
+
 //Usando get y set para asignar y validar las variables
         patient.setPhoneNumber("12345678");
         System.out.println(patient.getPhoneNumber());
