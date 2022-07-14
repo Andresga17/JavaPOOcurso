d276e4abe926b181227534901181783c612fc9be
Andresga17 <mariaerq10@gmail.com>
2022-07-14 17:54:15 -0500

    Haciendo de la clase User una clase abstracta

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import model.Doctor;
 import model.Patient;
+import model.User;
 
 import java.util.Date;
 
@@ -24,6 +25,7 @@ public class Main {
         myDoctor.addAvailableAppointment(new Date(),"1:00 p.m");
 
         System.out.println(myDoctor);
+
 
         /*for (model.Doctor.AvailableAppointment aA : myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + " " + aA.getTime());
